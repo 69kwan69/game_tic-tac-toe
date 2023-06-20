aac7e5b0a544c64302ba9f2528b82c0db8c6e8a9
69kwan69 <69hoangphi69@gmail.com>
2023-06-20 16:39:19 +0700

    update the game visual

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,14 +1,17 @@
 const player = (name, mark) => {
+    let score = 0;
     const getName = () => name;
-    const getMark = () => mark
-    return { getName, getMark }
+    const getMark = () => mark;
+    const getScore = () => score;
+    const winGame = () => score++;
+    return { getName, getMark, getScore, winGame }
 }
 
 
 const gameBoard = (() => {
     const board = ['', '', '', '', '', '', '', '', ''];
 
-    const resetBoard = () => board.forEach((item, index) => board[index] = '')
+    const resetGameBoard = () => board.forEach((item, index) => board[index] = '')
 
     const updateCell = (item, index) => {
         if (board[index]) return false;
@@ -16,14 +19,14 @@ const gameBoard = (() => {
         return true;
     }
 
-    const checkBoardFull = () => {
+    const checkgameBoardFull = () => {
         if (board.every(cell => cell !== '')) return true;
         return false;
     }
 
-    const getBoard = () => board;
+    const getGameBoard = () => board;
 
-    return { updateCell, resetBoard, checkBoardFull, getBoard }
+    return { updateCell, resetGameBoard, checkgameBoardFull, getGameBoard }
 })()
 
 
@@ -34,7 +37,7 @@ const gameFlow = (() => {
     let currentPlayer = player1;
 
     const checkWin = () => {
-        const board = gameBoard.getBoard();
+        const board = gameBoard.getGameBoard();
         const mark = currentPlayer.getMark();
 
         if (
@@ -70,8 +73,9 @@ const gameFlow = (() => {
                 board[7] === mark &&
                 board[8] === mark)
         ) {
+            currentPlayer.winGame();
             return true;
-        } else if (gameBoard.checkBoardFull()) {
+        } else if (gameBoard.checkgameBoardFull()) {
             return 'draw';
         }
 
@@ -81,6 +85,11 @@ const gameFlow = (() => {
     const switchPlayer = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
     }
+
+    const getPlayer1 = () => player1;
+    const getPlayer2 = () => player2;
+
+    const getCurrentPlayerName = () => currentPlayer.getName();
 
     const playerMove = (position) => {
         const name = currentPlayer.getName();
@@ -92,27 +101,83 @@ const gameFlow = (() => {
         }
     }
 
-    return { playerMove }
+    return { getPlayer1, getPlayer2, getCurrentPlayerName, playerMove }
 })()
 
 
 const updateDisplay = (() => {
-    const board = [...document.querySelectorAll('[data-position]')];
+    // HTML Elements
+    const board = document.querySelector('.board');
+    const cells = board.querySelectorAll('[data-position]')
+    const player1 = document.querySelector('.p1');
+    const player2 = document.querySelector('.p2');
+    const messageDialog = document.querySelector('dialog');
+    const rematchBtn = messageDialog.querySelector('.rematch');
 
-    board.forEach(cell => {
-        cell.addEventListener('click', function () {
-            const { name, mark, isWinner } = gameFlow.playerMove(this.dataset.position);
-            this.textContent = mark;
-            if (isWinner === true) {
-                setTimeout(() => { alert(`${name} win!`) }, 1);
-                gameBoard.resetBoard();
-                board.forEach((cell, index) => cell.textContent = gameBoard.getBoard()[index]);
-            } else if (isWinner === 'draw') {
-                setTimeout(() => { alert('Draw!') }, 1);
-                gameBoard.resetBoard();
-                board.forEach((cell, index) => cell.textContent = gameBoard.getBoard()[index]);
-            }
-        })
-    })
+    // Functions
+    const displayMark = (mark, position) => {
+        if (mark === 'x') {
+            cells[position].innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        } else if (mark === 'o') {
+            cells[position].innerHTML = '<i class="fa-solid fa-o"></i>';
+        }
+    }
 
+    const highlightCurrentPlayer = () => {
+        const player = gameFlow.getCurrentPlayerName();
+
+        if (player === player1.querySelector('select').value) {
+            player1.classList.remove('filter-grey');
+            player2.classList.add('filter-grey');
+        } else {
+            player1.classList.add('filter-grey');
+            player2.classList.remove('filter-grey');
+        }
+
+    }
+
+    const updatePlayerScore = () => {
+        player1.querySelector('.score').textContent = gameFlow.getPlayer1().getScore();
+        player2.querySelector('.score').textContent = gameFlow.getPlayer2().getScore();
+    }
+
+    const displayMessage = (name, isWin) => {
+        if (isWin === false) {
+            return;
+        } else if (isWin === true) {
+            messageDialog.querySelector('.message').textContent = `${name} won!`;
+            updatePlayerScore(name);
+        } else {
+            messageDialog.querySelector('.message').textContent = `It's a draw!`;
+        }
+
+        setTimeout(() => {
+            messageDialog.show();
+            board.classList.add('unclickable');
+        }, 150);
+    }
+
+    const resetBoard = () => {
+        gameBoard.resetGameBoard();
+        gameBoard.getGameBoard().forEach((cell, index) => cells[index].textContent = cell);
+    }
+
+    const confirmRematch = () => {
+        messageDialog.close();
+        board.classList.remove('unclickable');
+        resetBoard();
+    }
+
+    // Events
+    board.addEventListener('click', e => {
+        const position = e.target.dataset.position;
+        if (position) {
+            const { name, mark, isWinner } = gameFlow.playerMove(position);
+            displayMark(mark, position);
+            displayMessage(name, isWinner);
+            highlightCurrentPlayer();
+        }
+    });
+
+    rematchBtn.addEventListener('click', confirmRematch)
 })()
